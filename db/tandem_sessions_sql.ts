cfa55632083e51a1305d76941432fe124ce17942
Mohamed Mostafa <82705935+SoloAdventurer@users.noreply.github.com>
2025-12-13 17:14:20 +0200

    Frontend-testing now up to date with the latest changes of main

--- db/tandem_sessions_sql.ts
+++ db/tandem_sessions_sql.ts
@@ -1,40 +1,47 @@
-import { QueryArrayConfig, QueryArrayResult } from "pg";
+import { type QueryArrayConfig, type QueryArrayResult } from "pg";
 
-import { IPostgresInterval } from "postgres-interval";
+import { type IPostgresInterval } from "postgres-interval";
 
 interface Client {
-    query: (config: QueryArrayConfig) => Promise<QueryArrayResult>;
+  query: (config: QueryArrayConfig) => Promise<QueryArrayResult>;
 }
 
 export const retrieveUsersQuery = `-- name: retrieveUsers :one
 select session_id, status, start_time, scheduled_duration from tandem_session where session_id = $1`;
 
 export interface retrieveUsersArgs {
-    sessionId: string;
+  sessionId: string;
 }
 
 export interface retrieveUsersRow {
-    sessionId: string;
-    status: string | null;
-    startTime: Date;
-    scheduledDuration: IPostgresInterval;
+  sessionId: string;
+  status: string | null;
+  startTime: Date;
+  scheduledDuration: IPostgresInterval;
 }
 
-export async function retrieveUsers(client: Client, args: retrieveUsersArgs): Promise<retrieveUsersRow | null> {
-    const result = await client.query({
-        text: retrieveUsersQuery,
-        values: [args.sessionId],
-        rowMode: "array"
-    });
-    if (result.rows.length !== 1) {
-        return null;
-    }
-    const row = result.rows[0];
-    return {
-        sessionId: row[0],
-        status: row[1],
-        startTime: row[2],
-        scheduledDuration: row[3]
-    };
-}
+export async function retrieveUsers(
+  client: Client,
+  args: retrieveUsersArgs,
+): Promise<retrieveUsersRow | null> {
+  const result = await client.query({
+    text: retrieveUsersQuery,
+    values: [args.sessionId],
+    rowMode: "array",
+  });
+  if (result.rows.length !== 1) {
+    return null;
+  }
+  const row = result.rows[0];
 
+  if (row == undefined) {
+    return null;
+  }
+
+  return {
+    sessionId: row[0],
+    status: row[1],
+    startTime: row[2],
+    scheduledDuration: row[3],
+  };
+}
